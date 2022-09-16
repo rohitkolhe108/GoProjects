821a527fda30f945481c97f87b53f00b8544b411
kolhe <rohit.kolhe@adesso-service.com>
2022-09-16 18:23:46 +0200

    Add module example

--- module/submod/sub.go
+++ module/submod/sub.go
@@ -5,3 +5,8 @@ import "fmt"
 func SubPrint(){
 	fmt.Print("sub1")
 }
+
+func SubCall(moduleName string){
+	fmt.Print("Call from "+ moduleName +"submodule1")
+}
+
--- module/submod2/sub2.go
+++ module/submod2/sub2.go
@@ -1,7 +1,14 @@
 package submod
 
-import "fmt"
+import (
+	"fmt"
+	sb "github.com/rohitkolhe108/GoProjects/module/submod"
+)
 
 func SubPrint(){
 	fmt.Print("sub")
+}
+
+func SubCall2(moduelName string){
+	sb.SubCall("submodule2")
 }
